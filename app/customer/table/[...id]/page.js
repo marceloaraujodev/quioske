935e0f3cfda68869b444f5708fcfd66050a5afdc
marceloaraujodev <codetrsh@gmail.com>
2024-11-07 19:50:26 -0300

    customer menu page view fixed to server component and cardapio to client

--- app/customer/table/[...id]/page.js
+++ app/customer/table/[...id]/page.js
@@ -1,9 +1,9 @@
-'use client'
+
 import Cardapio from "@/app/components/Cardapio/Cardapio";
 
 export default async function Menu({params}) {
   const {id} = await params;
-  const [quioskeName, tableNumber, _id] = id[0].split("_");
+  const [ tableNumber, quioskeName, _id] = id[0].split("_");
 
   console.log(id);
   console.log(quioskeName, tableNumber, _id);
