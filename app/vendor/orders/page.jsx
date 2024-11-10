9dd1128ff99fa622772e22100f6ec422637bc0a7
marceloaraujodev <codetrsh@gmail.com>
2024-11-10 10:18:34 -0300

    orders

--- app/vendor/orders/page.jsx
+++ app/vendor/orders/page.jsx
@@ -1,21 +1,42 @@
-import c from './orders.module.css';
+"use client";
+import { useState, useEffect } from "react";
+import c from "./orders.module.css";
+import { use } from "bcrypt/promises";
 
 export default function OrdersPage() {
-  const orders = [
-    { order: 'brahma lata' },
-    { order: 'caipirinha limao' },
-    { order: 'skol garrafa' },
-    { order: 'heineken garrafa' },
-    { order: 'caipirinha limao' },
-    { order: 'skol garrafa' },
-    { order: 'heineken garrafa' },
-  ];
-  const filledOrders = [
-    { order: 'brahma lata' },
-    { order: 'caipirinha limao' },
-    { order: 'skol garrafa' },
-    { order: 'heineken garrafa' },
-  ];
+  const [orders, setOrder] = useState([
+    { order: "brahma lata" },
+    { order: "caipirinha limao" },
+    { order: "skol garrafa" },
+    { order: "heineken garrafa" },
+    { order: "caipirinha limao" },
+    { order: "skol garrafa" },
+    { order: "heineken garrafa" },
+  ]);
+  const [filledOrders, setFilledOrders] = useState([
+    { order: "caipirinha limao" },
+    { order: "skol garrafa" },
+    { order: "brahma lata" },
+    { order: "caipirinha limao" },
+    { order: "skol garrafa" },
+    { order: "heineken garrafa" },
+    { order: "heineken garrafa" },
+  ]);
+
+  useEffect(() => {
+
+  }, [orders])
+
+
+  function handleCompletedOrders(order, index){
+    console.log(order, index);
+    setOrder((prevOrders) => {
+      console.log(prevOrders)
+      
+      
+      return prevOrders.filter((item) => item !== order)
+    })
+  }
 
   return (
     <>
@@ -31,7 +52,7 @@ export default function OrdersPage() {
                       <div className={c.item}>{order.order}</div>
                     </div>
                     <div className={c.item}>
-                      <button className={c.btn}>Pronta</button>
+                      <button className={c.btn} onClick={() => handleCompletedOrders(order, index)}>Pronta</button>
                     </div>
                   </div>
                 </div>
@@ -40,26 +61,26 @@ export default function OrdersPage() {
           </div>
         </div>
       </div>
-     
+
       <h2>Completas</h2>
-        <div className={c.wrapper}>
-          <div className={c.filled}>
-            {orders.map((order, index) => {
-              return (
-                <div key={index} className={`${c.order} ${c.finished}`}>
-                  <div className={c.orderBlock}>
-                    <div className={c.item}>
-                      <div className={c.item}>{order.order}</div>
-                    </div>
-                    <div className={c.item}>
-                      <button className={`${c.btn} ${c.btnFinished}`}>Pronta</button>
-                    </div>
+      <div className={c.wrapper}>
+        <div className={c.filled}>
+          {filledOrders.map((order, index) => {
+            return (
+              <div key={index} className={`${c.order} ${c.finished}`}>
+                <div className={c.orderBlock}>
+                  <div className={c.item}>
+                    <div className={c.item}>{order.order}</div>
+                  </div>
+                  <div className={c.item}>
+                    <button className={`${c.btn} ${c.btnFinished}`}>Pronta</button>
                   </div>
                 </div>
-              );
-            })}
-          </div>
+              </div>
+            );
+          })}
         </div>
+      </div>
     </>
   );
 }
