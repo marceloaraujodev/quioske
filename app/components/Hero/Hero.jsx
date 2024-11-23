65c3543b670cde0c4da041ce01398cfbe2918938
codetrsh <134286416+codetrsh@users.noreply.github.com>
2024-11-23 12:25:02 -0300

    hero btns

--- app/components/Hero/Hero.jsx
+++ app/components/Hero/Hero.jsx
@@ -1,9 +1,26 @@
-import c from './Hero.module.css';
+import Link from "next/link";
 import { MdArrowRightAlt } from "react-icons/md";
+import contactUs from "@/app/utils/whatsapp";
+import c from './Hero.module.css';
 
 import React from 'react'
 
 export default function Hero() {
+  const handleScroll = (event, targetId) => {
+    event.preventDefault(); // Prevent default link behavior
+    
+    const targetElement = document.querySelector(targetId); // Select the target element
+    const openMenuSize = 100; // Dynamic menu size to deduct
+    const offset = targetElement.offsetTop - openMenuSize; // Calculate offset
+    
+    console.log(targetElement.offsetTop);
+
+    // Scroll to the calculated position
+    window.scrollTo({
+      top: offset,
+      behavior: 'smooth', // Smooth scroll
+    });
+  };
   return (
     <div className={c.cont}>
       <div className={c.leftBlock}>
@@ -12,10 +29,10 @@ export default function Hero() {
         </div>
         <p>Seamless menu management tailored to your bar and restaurant priorities</p>
         <div className={c.cta}>
-          <button>Saiba mais</button>
-          <a className={c.link}>
+          <button onClick={contactUs}>Saiba mais</button>
+          <Link className={c.link} href='#recursos' onClick={(e) => handleScroll(e, "#recursos")}>
           <span className={c.how}>como funciona</span> <MdArrowRightAlt size={25}/>
-          </a>
+          </Link>
         </div>
       </div>
       <div className={c.rightBlock}>
--- app/utils/whatsapp.js
+++ app/utils/whatsapp.js
@@ -1,6 +1,6 @@
 export default function contactUs(){
   const phoneNumber = "5547992009823"; // Add the recipient's phone number here
-  const message = "Olá tudo bem? Como podemos te ajudar?"; // Customize your message here
+  const message = "Olá tudo bem? A Quioske agradeçe o seu contato, como podemos te ajudar?"; // Customize your message here
   const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
   
   window.open(url, "_blank");
